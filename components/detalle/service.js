752ae957c658c07a4032c6364fcad5e6c384585a
valenkloster <valekloster18@gmail.com>
2025-01-03 10:44:02 -0300

    controlador para actualizar pedido

--- components/detalle/service.js
+++ components/detalle/service.js
@@ -9,12 +9,12 @@ class ProductDetailService {
 
     if (!id_producto || !tamanio || !newStock) {
       throw new Error(
-        'Parámetros inválidos. Verifica id_producto, tamanio y newStock.',
+        'Invalid parameters. Check product_id, size and newStock.',
       );
     }
     const productos = await product.get(filters);
     if (!productos.length) {
-      throw new Error('Producto no encontrado.');
+      throw new Error('Product not found.');
     }
     const producto = productos[0];
     const detalle = producto.detalles.find(
@@ -24,11 +24,45 @@ class ProductDetailService {
     );
 
     if (!detalle) {
-      throw new Error('Detalle del producto no encontrado.');
+      throw new Error('Detail not found.');
     }
 
     detalle.detalle.stock = newStock;
     await detalle.detalle.save();
+    return detalle.detalle;
+  }
+
+  async actualizarStock(filters, cantidad) {
+    const { id_producto, tamanio, tono_color } = filters;
+
+    if (!id_producto || !tamanio || !cantidad) {
+      throw new Error(
+        'Invalid parameters. Check product_id, size and quantity.',
+      );
+    }
+
+    const productos = await product.get(filters);
+    if (!productos.length) {
+      throw new Error('Product not found.');
+    }
+    const producto = productos[0];
+    const detalle = producto.detalles.find(
+      (d) =>
+        d.detalle.tamanio === tamanio &&
+        (tono_color ? d.detalle.tono_color === tono_color : true),
+    );
+
+    if (!detalle) {
+      throw new Error('Detail not found.');
+    }
+
+    const nuevoStock = detalle.detalle.stock - cantidad;
+    if (nuevoStock < 0) {
+      throw new Error('El stock no puede ser negativo.');
+    }
+    detalle.detalle.stock = nuevoStock;
+    await detalle.detalle.save();
+
     return detalle.detalle;
   }
 }
